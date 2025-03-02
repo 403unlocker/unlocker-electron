53ba1a143d59f19f3ebeac6acb04e91307d15dbe
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2025-03-03 00:57:45 +0330

    feat: add scrollable table with sticky headers

--- src/components/ResultTable.tsx
+++ src/components/ResultTable.tsx
@@ -1,8 +1,9 @@
 import { z } from "zod";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { twJoin } from "tailwind-merge";
 import { RiCloudOffLine } from "react-icons/ri";
 import { AiFillThunderbolt } from "react-icons/ai";
+import ScrollBox from "./ScrollBox";
 
 type Props = {
   link: string;
@@ -12,6 +13,8 @@ const validator = z.string().url();
 
 function ResultTable(props: Props) {
   const { link: unsafeLink } = props;
+
+  const [thBgEnabled, setThBgEnabled] = useState(false);
   const safeLink = useMemo(() => validator.safeParse(unsafeLink), [unsafeLink]);
 
   if (!safeLink.success)
@@ -28,7 +31,7 @@ function ResultTable(props: Props) {
     );
 
   return (
-    <div className="mt-16">
+    <div className="flex flex-col items-stretch mt-16 grow overflow-hidden">
       <div className="flex items-center gap-1 mb-2">
         <AiFillThunderbolt size={14} className="text-primary" />
         <p className="text-typo-0 text-sm">نتایج</p>
@@ -36,40 +39,45 @@ function ResultTable(props: Props) {
       <p className="text-xs font-medium text-typo-2 mb-8">
         اولین DNS بیشترین سرعت را دارد.
       </p>
-      <table className="w-full">
-        <thead>
-          <tr
-            className={twJoin(
-              "text-sm text-typo-2 [&>th]:py-3 [&>th]:text-right",
-              "[&>th]:font-normal [&>th]:border-b [&>th]:border-solid",
-              "[&>th]:border-[#232323] [&>th]:first:pb-2",
-            )}
-          >
-            <th>#</th>
-            <th>نام</th>
-            <th>دی‌ان‌اس</th>
-            <th>سرعت</th>
-            <th>وضعیت</th>
-          </tr>
-        </thead>
-        <tbody>
-          {new Array(20).fill(null).map((_, index) => (
+
+      <ScrollBox onScrollYPassed={setThBgEnabled}>
+        <table className="relative z-0 w-full">
+          <thead>
             <tr
-              key={index}
               className={twJoin(
-                "text-typo-0 text-sm [&>td]:py-3 [&>td]:border-b",
-                "[&>td]:border-solid [&>td]:border-[#232323] last:[&>td]:border-b-0",
+                "text-sm text-typo-2 [&>th]:py-3 [&>th]:text-right",
+                "[&>th]:font-normal [&>th]:border-b [&>th]:border-solid",
+                "[&>th]:border-[#232323] [&>th]:first:pb-2 [&>th]:sticky",
+                "[&>th]:top-0 z-[1] [&>th]:transition-colors [&>th]:duration-500",
+                thBgEnabled && "[&>th]:bg-paper/50 [&>th]:backdrop-blur-md",
               )}
             >
-              <td>{index + 1}</td>
-              <td>شکن</td>
-              <td>178.22.122.100</td>
-              <td>25MB/s</td>
-              <td className="text-success">آنلاین</td>
+              <th>#</th>
+              <th>نام</th>
+              <th>دی‌ان‌اس</th>
+              <th>سرعت</th>
+              <th>وضعیت</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {new Array(20).fill(null).map((_, index) => (
+              <tr
+                key={index}
+                className={twJoin(
+                  "text-typo-0 text-sm [&>td]:py-3 [&>td]:border-b",
+                  "[&>td]:border-solid [&>td]:border-[#232323] last:[&>td]:border-b-0",
+                )}
+              >
+                <td>{index + 1}</td>
+                <td>شکن</td>
+                <td>178.22.122.100</td>
+                <td>25MB/s</td>
+                <td className="text-success">آنلاین</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </ScrollBox>
     </div>
   );
 }
