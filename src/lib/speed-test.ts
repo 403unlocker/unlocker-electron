a12d5b77bb007b80f9d580c2bb90cc41a33795aa
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2025-03-14 02:31:42 +0330

    feat: add resolver and speed test features (WIP)

---
+++ src/lib/speed-test.ts
@@ -0,0 +1,54 @@
+import https from "https";
+import { IncomingMessage } from "http";
+import { Resolver } from "dns/promises";
+
+/** in seconds */
+const testTime = 10;
+
+const resolver = new Resolver();
+const defaultServers = resolver.getServers();
+
+/**
+ * Measure download speed using the provided dns server.
+ * @param dns - This function will make request to the url using provided dns server.
+ * @param url - Desired url to make request to it using provided dns server.
+ * @returns `>= 0` if the test is successful, otherwise `-1`.
+ */
+async function getSpeed(dns: string, url: string | URL): Promise<number> {
+  let fileSize = 0;
+  url = new URL(url);
+  resolver.setServers([dns]);
+
+  try {
+    const addresses = await resolver.resolve4(url.hostname);
+    const ip = addresses[0];
+
+    return new Promise<number>((resolve) => {
+      const requestOptions: https.RequestOptions = {
+        port: 443,
+        hostname: ip,
+        method: "HEAD",
+        path: url.pathname,
+        headers: { Host: url.hostname },
+      };
+
+      const responseHandler = (res: IncomingMessage) => {
+        const contentLength = res.headers["content-length"];
+        if (contentLength === undefined) return resolve(-1);
+        fileSize = parseInt(contentLength, 10);
+        console.log(fileSize, testTime);
+      };
+
+      const request = https.request(requestOptions, responseHandler);
+      request.on("error", () => resolve(-1));
+      request.end();
+    });
+  } catch (error) {
+    console.error("DNS Resolution Error:", error);
+    return -1;
+  } finally {
+    resolver.setServers(defaultServers);
+  }
+}
+
+export { getSpeed };
