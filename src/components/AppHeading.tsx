4a56afbcc9072aa677025c6205a66e11bd2226ce
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2025-03-20 23:02:37 +0330

    fix: replace anchor tags in header with button for opening external links

--- src/components/AppHeading.tsx
+++ src/components/AppHeading.tsx
@@ -5,18 +5,29 @@ const BUTTON_CS =
   "p-2 rounded-lg text-icon bg-icon-bg border border-solid " +
   "border-icon-border cursor-pointer";
 
+const X_LINK = "https://x.com/403unlocker";
+const GITHUB_LINK = "https://github.com/403unlocker/unlocker-electron";
+
 function AppHeading() {
+  const { api } = window;
+
   return (
     <header className="flex items-center gap-2">
       <img src={LogoSrc} alt="logo" />
       <h1 className="font-black text-base mt-1">۴۰۳،آنلاکر</h1>
 
-      <a href="https://x.com/403unlocker" target="_blank" className={`mr-auto ${BUTTON_CS}`}>
+      <button
+        className={`mr-auto ${BUTTON_CS}`}
+        onClick={() => api.openExternalLink(X_LINK)}
+      >
         <FaXTwitter size={14} />
-      </a>
-      <a href="https://github.com/403unlocker/unlocker-electron" target="_blank" className={`mr-1 ${BUTTON_CS}`}>
+      </button>
+      <button
+        className={`mr-1 ${BUTTON_CS}`}
+        onClick={() => api.openExternalLink(GITHUB_LINK)}
+      >
         <FaGithub size={14} />
-      </a>
+      </button>
     </header>
   );
 }
